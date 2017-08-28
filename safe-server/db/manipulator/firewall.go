6eadd67a1d8fc6b1e6d575a5e2e5cc00e8f311c4
zuiwuchang <zuiwuchang@gmail.com>
2017-08-28 16:33:01 +0800

    fix firewall.go has

--- safe-server/db/manipulator/firewall.go
+++ safe-server/db/manipulator/firewall.go
@@ -5,8 +5,22 @@ import (
 	"os/exec"
 )
 
+func Has(name string) bool {
+	str := fmt.Sprintf(`netsh advfirewall firewall show rule name="%v" dir=in`,
+		name,
+	)
+	cmd := exec.Command("cmd", "/C", str)
+	e := cmd.Run()
+	if e != nil {
+		return false
+	}
+	return true
+}
 func Add(ip string) error {
 	name := fmt.Sprintf("0-no-%v", ip)
+	if Has(name) {
+		return nil
+	}
 	str := fmt.Sprintf(`netsh advfirewall firewall add rule name="%v" remoteip="%v" dir=in action=block`,
 		name,
 		ip,
